d2771ee3f4a858ea8d524691bffbfbf9813e3c5b
Anshuman <tanshuman145@gmail.com>
2024-05-18 02:50:23 +0530

    changes did

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import QRCodeVerification from "./pages/QR";
+import SignUp from "./pages/Signup";
+import Login from "./pages/Login";
+import NotificationSettings from "./pages/Notification";
+import CustomerVoices from "./pages/Contact";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route
+          path="/scanqr"
+          element={<QRCodeVerification></QRCodeVerification>}
+        ></Route>
+        <Route path="/user/signup" element={<SignUp></SignUp>}></Route>
+        <Route path="/user/login" element={<Login></Login>}></Route>
+        <Route
+          path="/contact"
+          element={<CustomerVoices></CustomerVoices>}
+        ></Route>
+        <Route
+          path="/user/notifications"
+          element={<NotificationSettings></NotificationSettings>}
+        ></Route>
+        <Route path="/about" element={<Home></Home>}></Route>
+      </Routes>
+    </>
+  );
+};
+
+export default App;
