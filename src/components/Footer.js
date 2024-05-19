fd95d82b86021b4e9b2392a6de17eba1767e69ef
Anshuman <tanshuman145@gmail.com>
2024-05-19 19:17:12 +0530

    footer bugs fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -6,13 +7,13 @@ const Footer = () => {
       <footer class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
           <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
-            <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+            <Link class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
               <img
                 src="https://i.postimg.cc/8PZbqb9Z/logo1.png"
                 className="h=20 w-20"
               ></img>{" "}
               <span class="ml-3 text-xl">Smart Saver</span>
-            </a>
+            </Link>
             <p class="mt-2 text-sm text-gray-500">
               {" "}
               Our team is here to assist you every step of the way.
@@ -21,77 +22,102 @@ const Footer = () => {
           <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
             <div class="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-                CATEGORIES
+                Navigate here
               </h2>
               <nav class="list-none mb-10">
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">Home</a>
+                  <Link to="/" class="text-gray-600 hover:text-gray-800">
+                    Home
+                  </Link>
                 </li>
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">About Us</a>
+                  <Link to="/about" class="text-gray-600 hover:text-gray-800">
+                    About Us
+                  </Link>
                 </li>
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">Scan QR</a>
+                  <Link to="/scanqr" class="text-gray-600 hover:text-gray-800">
+                    Scan QR
+                  </Link>
                 </li>
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">Contact Us</a>
+                  <Link to="/contact" class="text-gray-600 hover:text-gray-800">
+                    Contact Us
+                  </Link>
                 </li>
               </nav>
             </div>
             <div class="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-                CATEGORIES
+                Useful Links
               </h2>
               <nav class="list-none mb-10">
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">Home</a>
+                  <Link class="text-gray-600 hover:text-gray-800">
+                    Services
+                  </Link>
                 </li>
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">About Us</a>
+                  <Link class="text-gray-600 hover:text-gray-800">
+                    Products
+                  </Link>
                 </li>
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">Scan QR</a>
-                </li>
-                <li>
-                  <a class="text-gray-600 hover:text-gray-800">Contact Us</a>
+                  <Link class="text-gray-600 hover:text-gray-800">
+                    Sitemap Link
+                  </Link>
                 </li>
               </nav>
             </div>
             <div class="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-                CATEGORIES
+                Legal Information
               </h2>
               <nav class="list-none mb-10">
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">Home</a>
+                  <Link class="text-gray-600 hover:text-gray-800">
+                    Privacy Policy
+                  </Link>
                 </li>
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">About Us</a>
+                  <Link class="text-gray-600 hover:text-gray-800">
+                    Terms of service
+                  </Link>
                 </li>
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">Scan QR</a>
+                  <Link class="text-gray-600 hover:text-gray-800">
+                    Disclaimer
+                  </Link>
                 </li>
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">Contact Us</a>
+                  <Link class="text-gray-600 hover:text-gray-800">
+                    Cookie Policy
+                  </Link>
                 </li>
               </nav>
             </div>
             <div class="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-                CATEGORIES
+                FAQ's
               </h2>
               <nav class="list-none mb-10">
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">Home</a>
+                  <Link class="text-gray-600 hover:text-gray-800">
+                    Help Center
+                  </Link>
                 </li>
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">About Us</a>
+                  <Link class="text-gray-600 hover:text-gray-800">
+                    Live Chat Link
+                  </Link>
                 </li>
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">Scan QR</a>
-                </li>
-                <li>
-                  <a class="text-gray-600 hover:text-gray-800">Contact Us</a>
+                  <Link
+                    to="/user/review"
+                    class="text-gray-600 hover:text-gray-800"
+                  >
+                    Customer Review
+                  </Link>
                 </li>
               </nav>
             </div>
@@ -101,17 +127,17 @@ const Footer = () => {
           <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
             <p class="text-gray-500 text-sm text-center sm:text-left">
               © 2024 SmartSaver —
-              <a
+              <Link
                 href=""
                 rel="noopener noreferrer"
                 class="text-gray-600 ml-1"
                 target="_blank"
               >
-                @SmartSaver
-              </a>
+                Email Address: SmartSaver@gmail.com
+              </Link>
             </p>
             <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-              <a class="text-gray-500">
+              <Link class="text-gray-500">
                 <svg
                   fill="currentColor"
                   stroke-linecap="round"
@@ -122,8 +148,8 @@ const Footer = () => {
                 >
                   <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                 </svg>
-              </a>
-              <a class="ml-3 text-gray-500">
+              </Link>
+              <Link class="ml-3 text-gray-500">
                 <svg
                   fill="currentColor"
                   stroke-linecap="round"
@@ -134,8 +160,8 @@ const Footer = () => {
                 >
                   <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                 </svg>
-              </a>
-              <a class="ml-3 text-gray-500">
+              </Link>
+              <Link class="ml-3 text-gray-500">
                 <svg
                   fill="none"
                   stroke="currentColor"
@@ -148,8 +174,8 @@ const Footer = () => {
                   <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                   <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                 </svg>
-              </a>
-              <a class="ml-3 text-gray-500">
+              </Link>
+              <Link class="ml-3 text-gray-500">
                 <svg
                   fill="currentColor"
                   stroke="currentColor"
@@ -165,7 +191,7 @@ const Footer = () => {
                   ></path>
                   <circle cx="4" cy="4" r="2" stroke="none"></circle>
                 </svg>
-              </a>
+              </Link>
             </span>
           </div>
         </div>
