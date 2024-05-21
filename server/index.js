3bb379faaa35357d0e7b92ab2f730a318e8b00bf
parth3083 <parthbrajput30@gmail.com>
2024-05-22 00:53:16 +0530

    "Update"

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const contactModel=require("./models/Contact")
+const contactModel = require("./models/Contact");
 app.use(express.json());
 require("dotenv").config();
 const mongoose = require("mongoose");
 app.use(cors());
-
-
 
 app.get("/", (req, res) => {
   res.send("Working");
@@ -16,7 +14,6 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then((res) => console.log("Connection successful 🔥🔥🔥🔥"))
   .catch((error) => console.log(error));
-
 
 app.post("/contact", async (req, res) => {
   const { name, email, message } = req.body;
@@ -27,7 +24,7 @@ app.post("/contact", async (req, res) => {
   });
   await result.save();
   console.log(result);
-})
+});
 
 app.listen(3000, () => {
   console.log("http://localhost:3000");
