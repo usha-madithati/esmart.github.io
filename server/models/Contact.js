74f5885e04fb1e7e87bb92afb3b2b16880a68d42
parth3083 <parthbrajput30@gmail.com>
2024-05-22 00:54:41 +0530

    "Update"

--- server/models/Contact.js
+++ server/models/Contact.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
- 
+const mongoose = require("mongoose");
+
 const contactSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    message: String,
+  name: String,
+  email: String,
+  message: String,
 });
 
-const Contact = mongoose.model('Contact', contactSchema);
+const Contact = mongoose.model("Contact", contactSchema);
 
 module.exports = Contact;
