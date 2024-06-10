9fb5cdc77a42b0a8194686a1645242864de94a5e
Ansh101112 <tanshuman145@gmail.com>
2024-06-11 03:45:17 +0530

    PNF added

--- src/pages/PNF.js
+++ src/pages/PNF.js
@@ -1,25 +1,28 @@
-import React from 'react';
-
+import React from "react";
+import Navbar from "../components/Navbar";
 
 const NotFoundPage = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
-      <div className="text-center space-y-4">
-        <h1 className="text-9xl font-bold neon-text animate-pulse">404</h1>
-        <p className="text-2xl font-medium">Page Not Found</p>
-        <div className="flex flex-col items-center space-y-2">
-          <p className="text-lg">Let's save the earth and save energy!</p>
-          <p className="text-lg">Enjoy the greenery while you're here.</p>
+    <>
+      <Navbar></Navbar>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
+        <div className="text-center space-y-4">
+          <h1 className="text-9xl font-bold neon-text animate-pulse">404</h1>
+          <p className="text-2xl font-medium">Page Not Found</p>
+          <div className="flex flex-col items-center space-y-2">
+            <p className="text-lg">Let's save the earth and save energy!</p>
+            <p className="text-lg">Enjoy the greenery while you're here.</p>
+          </div>
+        </div>
+        <div className="mt-8 animate-bounce">
+          <div className="flex space-x-4">
+            <Tree />
+            <Tree />
+            <Tree />
+          </div>
         </div>
       </div>
-      <div className="mt-8 animate-bounce">
-        <div className="flex space-x-4">
-          <Tree />
-          <Tree />
-          <Tree />
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
