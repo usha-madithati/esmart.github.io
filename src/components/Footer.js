d9b6a688e65463e2540653bdc1b46bd85d1ba30b
soujanya <soujusonu20@gmail.com>
2024-07-02 22:56:44 +0530

    added links for email,facebook,insta

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+const emailAddress='SmartSaver@gmail.com';
 
 const Footer = () => {
   return (
@@ -128,7 +129,7 @@ const Footer = () => {
             <p class="text-gray-500 text-sm text-center sm:text-left">
               © 2024 SmartSaver —
               <Link
-                href=""
+                to={`mailto:${emailAddress}`} 
                 rel="noopener noreferrer"
                 class="text-gray-600 ml-1"
                 target="_blank"
@@ -137,7 +138,7 @@ const Footer = () => {
               </Link>
             </p>
             <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-              <Link class="text-gray-500">
+              <Link to="https://www.facebook.com/profile.php?id=61561813920306" class="text-gray-500">
                 <svg
                   fill="currentColor"
                   stroke-linecap="round"
@@ -161,7 +162,7 @@ const Footer = () => {
                   <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                 </svg>
               </Link>
-              <Link class="ml-3 text-gray-500">
+              <Link to="https://www.instagram.com/smart_saver_2k24/" class="ml-3 text-gray-500">
                 <svg
                   fill="none"
                   stroke="currentColor"
